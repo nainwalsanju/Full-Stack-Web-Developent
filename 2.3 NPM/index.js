fda53212de5d4df74b05d9aada04e55b3f40d252
sanjay <sanjaynainwal129@gmail.com>
2023-11-23 03:41:01 +0530

    leanr package manager

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname";
+
+var sillyname = generateName();
+
+console.log(`My Name is ${sillyname}`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+
+console.log(`I am ${name}`);
